a8ba7e9de0021317ad376f07163b8b1ed6a2fe90
Tiago Murakami <trmurakami@gmail.com>
2016-06-10 13:32:52 -0300

    Importação do tema

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Tema do SIBiUSP</title>
+        <?php include('inc/meta-header.php'); ?>
+    </head>
+    <body>
+        <?php include('inc/barrausp.php'); ?>
+        <div class="ui main container">
+            <?php include('inc/header.php'); ?>
+            <?php include('inc/navbar.php'); ?>
+            <div id="main">
+                
+
+
+            </div>            
+        </div>
+        <?php include('inc/footer.php'); ?>
+    </body>
+</html>
